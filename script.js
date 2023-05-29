6699de6bcd95b415fe7e3235f85bc3cabaf88bd0
Hector Mata <hrmb26@gmail.com>
2023-05-29 09:05:57 -0300

    redefined code

--- script.js
+++ script.js
@@ -1,52 +1,35 @@
-function playerSelection() {
-  let userInput = prompt("Make your selection (Rock, paper, scissor)");
-  if (typeof userInput !== "string") {
-    alert("Please enter a valid choice!");
-    playerSelection();
+function pSelection() {
+  let userInput = prompt("Enter a selection (rock, paper, scissor)");
+
+  userInput = userInput.toLowerCase();
+  console.log(userInput);
+
+  if (
+    userInput !== "rock" &&
+    userInput !== "paper" &&
+    userInput !== "scissor"
+  ) {
+    console.log("Enter a valid selection");
+    pSelection();
   } else {
     return userInput;
   }
 }
 
-function computerSelection() {
-  function getRandomNumber(min, max) {
-    // Generate a random number between 0 and 1
-    var random = Math.random();
-
-    // Scale the random number to fit within the desired range
-    var scaled = random * (max - min + 1);
-
-    // Shift the range to start from the minimum value
-    var result = Math.floor(scaled) + min;
-
-    return result;
-  }
-
-  // Example usage
-  var randomNumber = getRandomNumber(1, 3);
-
-  switch (randomNumber) {
+function cSelection() {
+  const randomSelection = Math.floor(Math.random() * 3) + 1;
+  switch (randomSelection) {
     case 1:
-      return "rock";
+      return "rock ";
       break;
     case 2:
       return "paper";
       break;
     case 3:
       return "scissor";
+    default:
       break;
   }
 }
 
-function game(computerSelection, playerSelection) {
-  let player = playerSelection().toLowerCase();
-  let computer = computerSelection();
-
-  if (player === "rock" && computer === "scissor") {
-    return console.log("Player wins");
-  } else if (player === "rock" && computer === "paper") {
-    console.log("Computer wins!");
-  } else {
-    console.log("It's a draw");
-  }
-}
+pSelection();
