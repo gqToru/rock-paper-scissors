86a150faafa7c2b8d5bea0ce7ed66ae506e84e24
Hector Mata <hrmb26@gmail.com>
2023-05-31 08:55:05 -0300

    refactoring code for UI

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/* In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
+
+    * For now, remove the logic that plays exactly five rounds.
+
+    * Create three buttons, one for each selection. 
+
+    * Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+
+    * Add a div for displaying results and change all of your console.logs into DOM methods.
+
+    * Display the running score, and announce a winner of the game once one player reaches 5 points.
+
+    * You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
+ */
+
 function pSelection() {
   let userInput = prompt("Enter a selection (rock, paper, scissor)");
 
@@ -58,18 +73,18 @@ function game() {
 let playerScore = 0;
 let computerScore = 0;
 
-for (let i = 0; i < 5; i++) {
-  let gameScore = game();
+// for (let i = 0; i < 5; i++) {
+//   let gameScore = game();
 
-  if (gameScore === 1) {
-    playerScore++;
-  } else if (gameScore === 2) {
-    computerScore++;
-  }
+//   if (gameScore === 1) {
+//     playerScore++;
+//   } else if (gameScore === 2) {
+//     computerScore++;
+//   }
 
-  console.log("player score: ", playerScore);
-  console.log("computer score: ", computerScore);
-}
+//   console.log("player score: ", playerScore);
+//   console.log("computer score: ", computerScore);
+// }
 
 if (playerScore > computerScore) {
   console.log("Player Wins!");
